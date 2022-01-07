c098ce8cbbda75494ca35aaaa4edb9fa961a2858
EvgeniyKokaiko <70027656+EvgeniyKokaiko@users.noreply.github.com>
2022-01-07 15:23:04 +0200

    Update TABLE.go

--- server/types/TABLE.go
+++ server/types/TABLE.go
@@ -1,5 +1,9 @@
 package typedDB
 
+import (
+	"server/models"
+)
+
 type Tables struct {
 	USERS string
 	POSTS string
@@ -8,4 +12,20 @@ type Tables struct {
 var TABLES = Tables{
 	USERS: "users",
 	POSTS: "posts",
+}
+
+
+type DBMethods interface {
+	CreateEmptyUser(*models.EmptyUser) error
+	SetupAccount(*models.User) error
+	Login(*models.EmptyUser) (models.EmptyUser, string)
+	Me(string) *models.User
+	Avatar(string) string
+	AddPost(string, map[string]interface{}) (string, error)
+	DeletePost(string, string) (string, error)
+	GetMyPosts(string) ([]*models.Post, error)
+	Logout(string) error
+	CheckToken(string) (string, error)
+	GetNewsLine(string, int) ([]*models.Post, int ,error)
+	GetUserDataWithPosts(string, int) (map[string]interface{}, error)
 }
